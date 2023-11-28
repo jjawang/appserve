b8a037dcb72b17c43ebbcb897cb3414589303c71
jjawang <chadae111@naver.com>
2023-11-29 06:04:15 +0900

    start project

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1716314",
+  key: "6d753c1a28cee8791eb7",
+  secret: "8b4c3ae2e469a6568875",
+  cluster: "ap3",
+  useTLS: true
+});
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+}))
+
+app.use(express.json())
+
+app.post('/api/messages', async (req, res) => {
+    await pusher.trigger("chat", "message", {
+        username: req.body.username,
+        message: req.body.message
+    });
+
+    res.json([]);
+})
+
+console.log('listening to port 8000');
+app.listen(8000)
